e1a70fb79cedac8460cc7a3b8afeb7b3dba572ca
IsaacGH <garciaisaac468@gmail.com>
2019-11-12 21:45:45 -0600

    add forgot password component to app.js

--- quetzal/src/App.js
+++ quetzal/src/App.js
@@ -3,8 +3,9 @@ import { Switch, Route } from 'react-router-dom'
 
 import * as ROUTES from './Routes'
 
-import Header from './components/common/header/index.jsx';
+import Header from './components/common/header/index.jsx'
 import Landing from './windows/home-page/index.jsx'
+import ForgotPassword from './windows/authentication/forgot-password'
 
 export default class App extends Component {
   constructor( props ) {
@@ -20,6 +21,7 @@ export default class App extends Component {
         <Header />
         <Switch>
           <Route exact path = { ROUTES.HOME } component = { Landing }/>
+          <Route exact path = { ROUTES.FORGOTPASSWORD } component = { ForgotPassword }/>
         </Switch>
       </div>
     )
